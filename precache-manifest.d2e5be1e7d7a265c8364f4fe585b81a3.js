8078c0ed06f73d729522b04e1e81713c596c179a
Raymond Nawara <rjnawara@gmail.com>
2019-05-18 11:49:06 -0400

    Updates

---
+++ precache-manifest.d2e5be1e7d7a265c8364f4fe585b81a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3baa4d813de85af3f3bb68aafa06e103",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/Robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d6c4790e8330750993e6",
+    "url": "/Robofriends/static/css/main.7ef4a983.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/Robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "d6c4790e8330750993e6",
+    "url": "/Robofriends/static/js/main.491452e6.chunk.js"
+  },
+  {
+    "revision": "47afcf7c1fdce8fc928a",
+    "url": "/Robofriends/static/js/runtime~main.4fea8ee0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
